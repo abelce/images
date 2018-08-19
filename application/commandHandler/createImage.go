28deb76fd5215381e2eebfee20ea7bbd0df87000
abelce <1061225829@qq.com>
2018-08-19 10:07:13 +0800

    调试从file服务发起的请求

--- application/commandHandler/createImage.go
+++ application/commandHandler/createImage.go
@@ -1,22 +1,22 @@
 package commandHandler
 
 import (
+	"fmt"
 	"image"
 	"os"
-	"fmt"
 
 	"images/application/command"
 	"images/domain/model"
 )
 
 type CreateImage struct {
-	ImageRepository    model.Repository
-	QueryService         model.QueryService
+	ImageRepository model.Repository
+	QueryService    model.QueryService
 }
 
 // (title, markdowncontent, private, tags, status, categories, typ, description string)
 func (h CreateImage) Handle(c command.CreateImage) (*model.Image, error) {
-	
+
 	// 设置图片的宽度，高度
 	file, _ := os.Open(c.Url)
 	img, str, err := image.DecodeConfig(file)
@@ -26,11 +26,11 @@ func (h CreateImage) Handle(c command.CreateImage) (*model.Image, error) {
 	}
 	c.Width = img.Width
 	c.Height = img.Height
-
+	fmt.Println(c.Width)
 	image := model.NewImage(c.Url, c.Width, c.Height)
 	err = h.ImageRepository.Save(image)
 	if err != nil {
 		return nil, err
 	}
-    return image, nil;
+	return image, nil
 }
